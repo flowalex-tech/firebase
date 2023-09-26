4ab8e011a8dabd29b2569cfcd67c787ce080d4d6
Alex Wolf <alex.wolf@oracle.com>
2023-09-25 19:12:15 -0500

    add message to DOM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,22 @@
 import Image from 'next/image'
 import { useState } from 'react'
 
+type Message = {
+  userName: string
+  test: string
+}
+
 export default function Home() {
   const [disaplyName, setDisplayName] = useState('');
-  const [messageText, setMessageText] = useState('');
+  const [messageText, setMessageFeed] = useState('');
+  const [messageFeed, setMessageText] = useState<Message[]>([])
   const sendMessage = () => {
     const newMessage = {
       userName: disaplyName,
       test: messageText
     };
-    console.log(newMessage);
+    // console.log(newMessage);
+    setMessageFeed([...messageFeed, newMessage])
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
